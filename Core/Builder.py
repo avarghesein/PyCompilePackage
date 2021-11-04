ce166b96fe4e604190a0df2fe1862f16dfdfd925
Abraham <10294917+avarghesein@users.noreply.github.com>
2021-11-04 16:32:27 +0530

    Lightweight Python Pre Compiler and Package (As single standalone executable)

---
+++ Core/Builder.py
@@ -0,0 +1,91 @@
+import compileall
+import Core.Utility as UTL
+import os
+from pathlib import Path
+from shutil import copyfile, rmtree
+from distutils.dir_util import copy_tree
+import fnmatch
+import zipapp
+
+
+def Shell(s):
+    import subprocess
+    return subprocess.check_output(s,shell=True)
+
+def BuildAll():
+
+    buildCfg = UTL.BuildConfig
+
+    srcDir = str(Path(buildCfg.SRC_DIR).absolute())
+    dstDir = str(Path(buildCfg.BLD_DIR).absolute())
+    if os.path.exists(dstDir):  rmtree(dstDir)
+    dstRoot = dstDir    
+    dstDir += "/OBJ/"
+    UTL.Trace("Compilation Started")
+    # Perform same compilation, excluding files in .svn directories.
+    import re
+    compileall.compile_dir(srcDir, rx=re.compile(r'[//][.]svn'), force=True, maxlevels=buildCfg.BLD_LEVEL, ddir=dstDir)
+
+    if not os.path.exists(dstDir): os.makedirs(dstDir, exist_ok=True)
+
+    def GetCompiledFiles(rootDir):
+        for root, dirs, files in os.walk(rootDir):
+            for dir in dirs:
+                GetCompiledFiles(f"{root}/{dir}")
+
+            for file in fnmatch.filter(files, "*.pyc"):
+                yield f"{root}/{file}" 
+
+    for compileFile in GetCompiledFiles(srcDir):
+        srcFile = compileFile
+
+        if dstRoot in srcFile:
+             continue
+
+        dstFile = srcFile.replace(srcDir,dstDir)
+        dstFile = dstFile.replace("__pycache__","")
+
+        dstPath,_,_ = UTL.GetPathFileExtension(dstFile)
+        if not os.path.exists(dstPath): os.makedirs(dstPath, exist_ok=True)
+        for objFileName in buildCfg.RENAME_OBJ_FILES:
+            if objFileName in dstFile:
+                dstFile = dstFile.replace(objFileName,".pyc")
+
+        if os.path.exists(dstFile): os.remove(dstFile)
+        copyfile(srcFile, dstFile)
+
+    UTL.Trace("Copying Resources")
+    for resource in buildCfg.COPY_RESOURCES:
+        src = f"{srcDir}/{resource[0]}"
+
+        if os.path.isdir(src):
+            dst = f"{dstDir}/{resource[1]}"
+            copy_tree(src, dst)
+
+            dst = f"{dstRoot}/{resource[1]}"
+            copy_tree(src, dst)
+            continue
+        
+        _,file,ext = UTL.GetPathFileExtension(src)
+        dst = f"{dstDir}/{resource[1]}/{file}{ext}"
+        if os.path.exists(dst): os.remove(dst)
+        dstPath,_,_ = UTL.GetPathFileExtension(dst)
+        if not os.path.exists(dstPath): os.makedirs(dstPath, exist_ok=True)
+        copyfile(src, dst)
+
+        dst = f"{dstRoot}/{resource[1]}/{file}{ext}"
+        if os.path.exists(dst): os.remove(dst)
+        dstPath,_,_ = UTL.GetPathFileExtension(dst)
+        if not os.path.exists(dstPath): os.makedirs(dstPath, exist_ok=True)
+        copyfile(src, dst)
+    
+    UTL.Trace("Building Package")
+    zipapp.create_archive(dstDir, f"{dstRoot}/{buildCfg.PACKAGE_NAME}.pyz",compressed=True,main=buildCfg.ENTRY_POINT)
+
+    if buildCfg.CLEANUP_OBJ_FILES == "YES":
+        UTL.Trace("Cleaning up")
+        for filename in os.listdir(dstDir):
+            if filename.endswith(".pyc"): 
+                os.remove(f'{dstDir}/{filename}')
+    
+        rmtree(dstDir)
---
+++ Core/Utility.py
@@ -0,0 +1,88 @@
+import os
+import sys
+import json
+from datetime import datetime, timedelta
+
+StatusFile = None
+LogFile = None
+TraceFile = None
+BuildConfig = None
+
+ProgramPath = None
+
+def GetNowUtc():
+    #return (datetime.utcnow().date()+ timedelta(days=1)).strftime("%Y-%m-%d %H:%M:%S")
+    return datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+def GetPathFileExtension(file):
+    filePath = os.path.split(file)
+    filenamewithext = os.path.basename(file)
+    filename, ext = os.path.splitext(filenamewithext)
+    return filePath[0], filename, ext
+
+def GetJsonAsObject(templatePath):
+    from types import SimpleNamespace
+    contents = ""
+    with open(templatePath) as f:
+        contents = f.read()
+        
+    obj = json.loads(contents, object_hook=lambda d: SimpleNamespace(**d))
+    return obj
+
+def GetEnv(key, defaultVal=""):
+    try:
+        return os.environ[key] if key in os.environ else defaultVal
+    except Exception as e:
+        Log(e)
+        return defaultVal
+
+def get_script_path():
+    return os.path.dirname(os.path.realpath(sys.argv[0]))
+
+
+def GetJsonVal(file,key, defaultVal = None):
+    try:
+        jsonObj = json.loads(open(file, 'r').read())
+        return jsonObj[key]
+    except Exception as e:
+        Log(e)
+        return defaultVal
+     
+
+def WriteStatus(status, exception = None):
+    global StatusFile
+    try:
+        with open(StatusFile, "w") as logFile:
+            logFile.write(status)
+            if not (exception is None):
+                logFile.write(":" + str(exception))
+
+            logFile.close()
+            
+    except Exception as e:
+        self.Log(e)     
+
+def Trace(message):
+    try:
+        print("Message->" + message)
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(TraceFile, "a") as logFile:
+            logFile.write(date + ":" +message +"\n")
+            logFile.close()
+    except Exception as e:
+        self.Log(e) 
+
+def Log(exception):
+    try:
+        print("Error->" + str(exception))
+        import sys
+        exc_type, exc_obj, exc_tb = sys.exc_info()
+        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+        msg=f"{exc_type},{fname},{exc_tb.tb_lineno}"
+        print(exc_type, fname, exc_tb.tb_lineno)
+        date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(LogFile, "a") as logFile:
+            logFile.write(date + ":" + str(exception) + ":" + msg +"\n")
+            logFile.close()
+    except Exception as e:
+        print(e)  
