b5818426165567a97cdc3b9dcf7086e3972bc7ed
avarghese.in <10294917+avarghesein@users.noreply.github.com>
2021-11-28 12:17:48 +0530

    BuiltIn Internal Resource Manager Support

--- DemoStandaloneApp/BUILD/BuildPackage.sh
+++ DemoStandaloneApp/BUILD/BuildPackage.sh
@@ -4,8 +4,8 @@ PYTHON_EXE_PATH=
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 BUILD_CONFIG_PATH=$SCRIPT_DIR
 PYTHONPATH=$BUILD_CONFIG_PATH
-BUILD_CONFIG="$BUILD_CONFIG_PATH/BuildConfig.json"
 
 cd $SCRIPT_DIR
 
+BUILD_CONFIG="$BUILD_CONFIG_PATH/BuildConfig.json" \
 ${PYTHON_EXE_PATH}python -O $BUILD_CONFIG_PATH/PyCompilePackager.pyz
