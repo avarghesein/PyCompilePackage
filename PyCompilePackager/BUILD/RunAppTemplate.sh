0c12c301c84bdf2190c19b3fca1c6d067ffe1905
avarghese.in <10294917+avarghesein@users.noreply.github.com>
2021-11-28 12:08:34 +0530

    Built In Extended Resource Manager Support

--- PyCompilePackager/BUILD/RunAppTemplate.sh
+++ PyCompilePackager/BUILD/RunAppTemplate.sh
@@ -4,7 +4,6 @@ PYTHON_EXE_PATH=
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
-LIB_PATH="${SCRIPT_DIR}/LIBS"
-
+LIB_PATH="${SCRIPT_DIR}/LIBS" \
 PYTHONPATH=$SCRIPT_DIR \
 "${PYTHON_EXE_PATH}python" -O $SCRIPT_DIR/MyPackage.pyz
